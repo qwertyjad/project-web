f37069272f5860254956e79806474f3dd856933a
qwertyjad <pofrancisjadei@gmail.com>
2025-03-11 08:22:27 +0000

    heheee

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    }
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  // Set a different port to avoid conflicts
+  serverOptions: {
+    port: 3001,
+  },
 };
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
-    nextConfig["experimental"] = {
-        // NextJS 13.4.8 up to 14.1.3:
-        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-        // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+  nextConfig["experimental"] = {
+    // NextJS 13.4.8 up to 14.1.3:
+    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+    // NextJS 14.1.3 to 14.2.11:
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
 
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
+    // NextJS 15+ (Not yet supported, coming soon)
+  };
 }
 
 module.exports = nextConfig;
